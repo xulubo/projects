6c9d6bdd0251f0f6e41bc617531f30eb42dfcd21
xulubo <xulubo@gmail.com>
2014-03-13 09:18:05 -0400

    just save code

--- Creacon/src/main/java/com/creacon/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.creacon;
-
-class HelloWorld {
-	public void Say(String words) {
-	
-	}
-}
---
+++ Creacon/ws-aar/src/main/java/com/creacon/Converter.java
@@ -0,0 +1,14 @@
+package com.creacon;
+
+public class Converter
+{
+  public float celsiusToFarenheit ( float celsius )
+  {
+    return (celsius * 9 / 5) + 32;
+  }
+
+  public float farenheitToCelsius ( float farenheit )
+  {
+    return (farenheit - 32) * 5 / 9;
+  }
+}
